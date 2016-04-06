ba674078cc2cfe4c03bd5fad2dc13f70b603dcb8
= <kristoffer@kristofferhebert.com>
2016-04-05 20:15:47 -0700

    feat: adding user schema

--- app/api/user/UserModel.js
+++ app/api/user/UserModel.js
@@ -1,1 +1,11 @@
 import db from '../db'
+
+const Schema = db.Schema
+const UserSchema = new Schema({
+    email: String,
+    password: String
+})
+
+const UserModel = db.model('user',UserSchema)  
+
+export default UserModel
