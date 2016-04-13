22e05e690c6ca38eb1aca9d1af6bd9f65df91511
= <kristoffer@kristofferhebert.com>
2016-04-12 17:59:28 -0700

    refactor: adding env to config and refactoring db selecotr

--- app/api/db.js
+++ app/api/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import config from '../config'
 
 // Set Database based on enviroment
-const database = (process.env.ENVIROMENT === 'production') ? config.live_db : config.staging_db
+const database = (config.env === 'production') ? config.live_db : config.staging_db
 
 let db = mongoose.createConnection('mongodb://localhost/' + database)
 
--- app/config.js
+++ app/config.js
@@ -1,7 +1,8 @@
 const config = {
     port: 8089,
     staging_db: 'kelp_staging',
-    live_db: 'kelp_live'
+    live_db: 'kelp_live',
+    env: (process.env.ENVIROMENT === 'production') ? 'production' : 'development'
 }
 
 export default config
