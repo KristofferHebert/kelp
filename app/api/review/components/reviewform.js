0db8bc3a12880079ddc3ccf48ef3ddc3905d9504
= <kristoffer@kristofferhebert.com>
2016-04-18 19:13:21 -0700

    feat: adding work on review form

---
+++ app/api/review/components/reviewform.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const ReviewForm = React.createClass({
+    setInitialState(){
+        return {
+            title: "",
+            owner_id: "",
+            restaurant_id: "",
+            body: "",
+            stars: ""
+        }
+    },
+    componentWillMount(){
+
+    },
+    getInitialProps(){
+
+    },
+    handleSubmit(e){
+        
+        e.preventDefault()
+    },
+    handleChange(e){
+
+        let updatedState = this.state[e.target.name]
+        updateState = e.target.value
+        this.setState(updateState)
+    },
+    render(){
+        return (
+            <form>
+                <label for="title">
+                    <input name="title" type="text" value={this.state.title} placeholder="Title" onChange={this.handleChange} required/>
+                </label>
+                <label for="title">
+                    <input name="stars" type="number" value={this.state.stars} placeholder="Stars" onChange={this.handleChange} min="1" max="5" required/>
+                </label>
+                <label for="body">
+                    <textarea name="body" placeholder="Body" onChange={this.handleChange} required>
+                        {this.state.body}
+                    </textarea>
+                </label>
+                <input name="submit" type="submit" />
+            </form>
+        )
+    }
+})
+
+export default ReviewForm
