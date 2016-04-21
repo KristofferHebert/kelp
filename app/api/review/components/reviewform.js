08725d88c28b9f483e6ada0c7068be76d80a29d7
= <kristoffer@kristofferhebert.com>
2016-04-20 20:38:18 -0700

    chore: tweaking whitespace based on eslint

--- app/api/review/components/reviewform.js
+++ app/api/review/components/reviewform.js
@@ -1,43 +1,41 @@
-import React function (arguments) {}rom 'react'
+import React from 'react'
 
 const ReviewForm = React.createClass({
-    setInitialState(){
-        return {
-            title: "",
-            owner_id: "",
-            restaurant_id: "",
-            body: "",
-            stars: ""
-        }
-    },
-    handleSubmit(e){
-
-        e.preventDefault()
-    },
-    handleChange(e){
-
-        let updatedState = this.state[e.target.name]
-        updateState = e.target.value
-        this.setState(updateState)
-    },
-    render(){
-        return (
-            <form {...this.props}>
-                <label for="title">
-                    <input name="title" type="text" value={this.state.title} placeholder="Title" onChange={this.handleChange} required/>
-                </label>
-                <label for="title">
-                    <input name="stars" type="number" value={this.state.stars} placeholder="Stars" onChange={this.handleChange} min="1" max="5" required/>
-                </label>
-                <label for="body">
-                    <textarea name="body" placeholder="Body" onChange={this.handleChange} required>
-                        {this.state.body}
-                    </textarea>
-                </label>
-                <input name="submit" type="submit" />
-            </form>
-        )
+  setInitialState () {
+    return {
+      title: '',
+      owner_id: '',
+      restaurant_id: '',
+      body: '',
+      stars: ''
     }
+  },
+  handleSubmit (e) {
+    e.preventDefault()
+  },
+  handleChange (e) {
+    let updatedState = this.state[e.target.name]
+    updatedState = e.target.value
+    this.setState(updatedState)
+  },
+  render () {
+    return (
+      <form {...this.props}>
+        <label for='title'>
+          <input name='title' type='text' value={this.state.title} placeholder='Title' onChange={this.handleChange} required/>
+        </label>
+        <label for='title'>
+          <input name='stars' type='number' value={this.state.stars} placeholder='Stars' onChange={this.handleChange} min='1' max='5' required/>
+        </label>
+        <label for='body'>
+          <textarea name='body' placeholder='Body' onChange={this.handleChange} required>
+            {this.state.body}
+          </textarea>
+        </label>
+        <input name='submit' type='submit' />
+      </form>
+      )
+  }
 })
 
 export default ReviewForm
