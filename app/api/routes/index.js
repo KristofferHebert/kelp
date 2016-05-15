9f972e41566a592cf17edebaed7f879f500ef151
= <kristoffer@kristofferhebert.com>
2016-05-15 09:56:35 -0700

    chore: removing unused files

--- app/api/routes/index.js
+++ app/api/routes/index.js
@@ -3,9 +3,9 @@ import Router from 'react-router'
 import UserRouter from '../user/userrouter'
 import ReviewRouter from '../review/reviewrouter'
 import RestaurantRouter from '../restaurant/restaurantrouter'
-import ServerRouter from './serverrouter'
 
 const routes = express.Router()
+// API endpoints
 routes.use('/api/user', UserRouter.UserAPIRouter)
 routes.use('/api/review', ReviewRouter)
 routes.use('/api/restaurant', RestaurantRouter.RestaurantAPIRouter)
