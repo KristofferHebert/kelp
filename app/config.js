bae7413b6d166e2cb1c4703d8e3d4c38d7e7965a
root <root@dev.(none)>
2016-05-19 02:20:15 +0000

    chore: updating prod command

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 const config = {
   port: 4084,
-  env: (process.env.ENVIROMENT === 'production') ? 'production' : 'development',
+  env: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
   keys: ['dX1eSTvHLuJBre1uTtUzuqm8', 'MJDVcAPODRe9D0UrFAgIUbH8', '...'],
-  database: (process.env.ENVIROMENT === 'production') ? 'kelp_live' : 'kelp_staging'
+  database: (process.env.NODE_ENV === 'production') ? 'kelp_production' : 'kelp_staging'
 }
 
 export default config
