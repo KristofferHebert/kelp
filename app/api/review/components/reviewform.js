a5238abc944f75252e9e67f782512d4e62e0e5a1
= <kristoffer@kristofferhebert.com>
2016-05-21 07:48:47 -0700

    refactor: adding aria roles

--- app/api/review/components/reviewform.js
+++ app/api/review/components/reviewform.js
@@ -9,9 +9,9 @@ const ReviewForm = React.createClass({
         <h4>Add Review</h4>
         <fieldset className='form-inline'>
           <label htmlFor='title' hidden>Title:</label>
-        <input name='title' type='text' value={this.props.newReview.title} placeholder='Title' onChange={this.props.handleChange} className='form-control' tabIndex='20' required/>
+        <input name='title' id='title' type='text' value={this.props.newReview.title} placeholder='Title' onChange={this.props.handleChange} className='form-control' tabIndex='20' required/>
           <label htmlFor='stars' className='ml'>Rating:</label>
-        <select className='form-control' name='stars' selected='1' value={this.props.newReview.stars} placeholder='Stars' onChange={this.props.handleChange} className='form-control' tabIndex='21'>
+        <select className='form-control' id='stars' name='stars' selected='1' value={this.props.newReview.stars} placeholder='Stars' onChange={this.props.handleChange} className='form-control' tabIndex='21'>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -20,17 +20,17 @@ const ReviewForm = React.createClass({
           </select>
         </fieldset>
         <fieldset className='form-group'>
-          <label htmlFor='message' hidden>Message</label>
-        <textarea name='body' placeholder='Message' onChange={this.props.handleChange} className='form-control' rows='3' value={this.props.newReview.body} tabIndex='22'required />
+          <label htmlFor='body' hidden>Message</label>
+        <textarea id='body' name='body' placeholder='Message' onChange={this.props.handleChange} className='form-control' rows='3' value={this.props.newReview.body} tabIndex='22'required />
         </fieldset>
-        <input name='submit' type='submit' className='btn btn-primary btn-block' tabIndex='23'/>
+        <input name='submit' type='submit' className='btn btn-primary btn-block' role='button' tabIndex='23'/>
       </form>
     )
   },
   renderCreateOrLogin () {
     return (
       <section className='padding bg-info text-center' tabIndex='40'>
-        <p>Please <a href='/user/login' className='btn btn-primary'>Login</a> or <a href='/user/signup' className='btn btn-primary' tabIndex='23'>Create Account</a></p>
+        <p>Please <a href='/user/login' className='btn btn-primary' role='button'>Login</a> or <a href='/user/signup' className='btn btn-primary' tabIndex='23'>Create Account</a></p>
       </section>
     )
   },
