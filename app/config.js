be1a47408a8564a40577a7c19d1590a6d566e38d
kristofferhebert <kristofferhebert>
2018-06-03 15:44:24 -0700

    Removing config.js from repo

--- app/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  port: 4084,
-  env: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
-  keys: ['dX1eSTvHLuJBre1uTtUzuqm8', 'MJDVcAPODRe9D0UrFAgIUbH8', '...'],
-  database: (process.env.NODE_ENV === 'production') ? 'kelp_production' : 'kelp_staging'
-}
-
-export default config
